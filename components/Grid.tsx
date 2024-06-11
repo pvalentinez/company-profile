8bfb4791792218498759c24ba3455fe02ac1756d
natapon <natapon@vekin.co.th>
2024-06-12 04:57:08 +0700

    bento grid

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
-import { BentoGrid } from './ui/BentoGrid'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { gridItems } from '@/data'
 
 const Grid = () => {
   return (
     <section id="about">
         <BentoGrid>
-            {[{ title: 'Title1'}]}
+            {gridItems.map
+            (({id, title, description, className, img, imgClassName, titleClassName, spareImg})=>(
+                <BentoGridItem 
+                id={id}
+                key={id}
+                title={title}
+                description={description}
+                className={className}
+                img={img}
+                imgClassName={imgClassName}
+                titleClassName={titleClassName}
+                spareImg={spareImg}
+                />
+            ))}
         </BentoGrid>
     </section>
   )
